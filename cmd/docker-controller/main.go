fbdcaa1d4991e8f557c06bb6f946c25f879ce549
vladislav <ve@incsub.com>
2023-04-21 22:09:44 +0200

    Use stub tasks getter

--- cmd/docker-controller/main.go
+++ cmd/docker-controller/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 
 	"github.com/docker/docker/client"
 )
@@ -14,8 +15,41 @@ func main() {
 		panic(err)
 	}
 
-	if err := Run(ctx, cli, 1312, "http://puppychowfoo.rocks"); err != nil {
-		log.Fatalf("Error running a task: %v (%T)", err, err)
+	tasks := getSitesToPing()
+
+	var wg sync.WaitGroup
+	wg.Add(len(tasks))
+
+	for _, task := range tasks {
+		go func(task ping) {
+			if err := Run(ctx, cli, task.SiteID, task.URL); err != nil {
+				log.Printf("Error running a task %v: %v (%T)", task, err, err)
+			}
+			wg.Done()
+		}(task)
 	}
+
+	wg.Wait()
 	log.Println("-- all good --")
+}
+
+type ping struct {
+	SiteID int
+	URL    string
+}
+
+func getSitesToPing() []ping {
+	// TODO: implement fetching pings
+	// This is going to be something like:
+	// SELECT * FROM sites WHERE toPing=1 AND somehow-last-pinged WITHIN <PING_INTERVAL+1>
+	return []ping{
+		ping{
+			SiteID: 1312,
+			URL:    "https://snap42.wpmudev.host",
+		},
+		ping{
+			SiteID: 161,
+			URL:    "http://puppychowfoo.rocks",
+		},
+	}
 }
