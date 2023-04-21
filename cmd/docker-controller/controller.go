7483bdf873c864c5410aea65100a39bd60280091
vladislav <ve@incsub.com>
2023-04-21 18:48:56 +0200

    Restart runaway containers

--- cmd/docker-controller/controller.go
+++ cmd/docker-controller/controller.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
@@ -18,12 +19,6 @@ func Run(ctx context.Context, siteID int, siteURL string) error {
 		return err
 	}
 
-	lnk, err := url.Parse(siteURL)
-	if err != nil {
-		return err
-	}
-
-	jobName := fmt.Sprintf("ping-%d-%s", siteID, strings.Replace(lnk.Host, ":", "-", -1))
 	ccfg := &container.Config{
 		Env: []string{
 			fmt.Sprintf("AVBL_SITE_ID=%d", siteID),
@@ -40,9 +35,28 @@ func Run(ctx context.Context, siteID int, siteURL string) error {
 		hcfg,
 		nil,
 		nil,
-		jobName)
+		getJobName(siteID, siteURL))
 	if err != nil {
-		return err
+		log.Printf("Error starting container: %v", err)
+		log.Println("Possibly a runaway task. Re-starting")
+		if err := Stop(ctx, siteID, siteURL); err != nil {
+			log.Println("Giving up")
+			return err
+		}
+
+		log.Println("Let's try create one more time")
+		resp, err = cli.ContainerCreate(
+			ctx,
+			ccfg,
+			hcfg,
+			nil,
+			nil,
+			getJobName(siteID, siteURL))
+		if err != nil {
+			log.Printf("Error re-attempting container create: %v", err)
+			log.Println("Giving up")
+			return err
+		}
 	}
 
 	log.Printf("Successfully created task: %#v", resp)
@@ -53,4 +67,36 @@ func Run(ctx context.Context, siteID int, siteURL string) error {
 
 	log.Println("Job started")
 	return nil
+}
+
+func Stop(ctx context.Context, siteID int, siteURL string) error {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	jobName := getJobName(siteID, siteURL)
+	tmout := 0
+	opts := container.StopOptions{Timeout: &tmout}
+	if err := cli.ContainerStop(ctx, jobName, opts); err != nil {
+		log.Printf("Error stopping container: %v", err)
+		return err
+	}
+
+	log.Println("Container apparently stopped")
+	time.Sleep(time.Second)
+	return nil
+}
+
+func getJobName(siteID int, siteURL string) string {
+	var b strings.Builder
+	fmt.Fprintf(&b, "ping-%d", siteID)
+
+	lnk, err := url.Parse(siteURL)
+	if err == nil {
+		b.WriteString("-")
+		b.WriteString(strings.Replace(lnk.Host, ":", "", -1))
+	}
+
+	return b.String()
 }
--- cmd/docker-controller/controller_test.go
+++ cmd/docker-controller/controller_test.go
@@ -2,5 +2,21 @@ package main
 
 import "testing"
 
-func Test_Controller(t *testing.T) {
+func Test_getJobName(t *testing.T) {
+	suite := map[string]string{
+		"http://puppychowfoo.rocks":      "ping-161-puppychowfoo.rocks",
+		"https://puppychowfoo.rocks":     "ping-161-puppychowfoo.rocks",
+		"http://puppy.chowfoo.rocks":     "ping-161-puppy.chowfoo.rocks",
+		"http://puppy.chow.foo.rocks":    "ping-161-puppy.chow.foo.rocks",
+		"http://puppychowfoo.rocks:80":   "ping-161-puppychowfoo.rocks80",
+		"http://puppychowfoo.rocks/test": "ping-161-puppychowfoo.rocks",
+	}
+	for test, want := range suite {
+		t.Run(test, func(t *testing.T) {
+			got := getJobName(161, test)
+			if want != got {
+				t.Errorf("want %s, got %s", want, got)
+			}
+		})
+	}
 }
