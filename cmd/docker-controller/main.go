ef47f19c6380b97e189b8a308c4d57ef63e1c83d
vladislav <ve@incsub.com>
2023-04-22 20:41:16 +0200

    Controller is now a scheduler

--- cmd/docker-controller/main.go
+++ cmd/docker-controller/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"sync"
+	"time"
 
 	"availability/pkg/data/collections"
 	"availability/pkg/data/model"
@@ -20,27 +21,41 @@ const (
 
 func main() {
 	ctx := context.Background()
+	for {
+		if err := run(ctx); err != nil {
+			panic(err)
+		}
+		time.Sleep(time.Duration(pingTimeoutSecs) * time.Second)
+	}
+}
+
+func run(ctx context.Context) error {
+	log.Println("task scheduling:")
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(pingTimeoutSecs)*time.Second)
+	defer cancel()
+
 	cli, err := client.NewEnvClient()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	query := new(sql.TaskCollection)
 	if err := query.Connect(); err != nil {
-		panic("unable to connect")
+		return err
 	}
 	defer query.Disconnect()
 
 	tmout := pingTimeoutSecs + maxResponseDurationSecs
 	tasks, err := collections.GetActiveTasks(query, maxActiveTasks, tmout)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	var wg sync.WaitGroup
 	wg.Add(len(tasks))
 
 	for _, t := range tasks {
+		log.Printf("\t- scheduling task %v", t)
 		go func(t *model.Task) {
 			if err := Run(ctx, cli, t); err != nil {
 				log.Printf("Error running a task %v: %v (%T)", t, err, err)
@@ -50,5 +65,6 @@ func main() {
 	}
 
 	wg.Wait()
-	log.Println("-- all good --")
+	log.Printf("done scheduling: %s", time.Now())
+	return nil
 }
