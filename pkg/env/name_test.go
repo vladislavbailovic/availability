e71c8dba92964068402e97ac217dc4b5e5122921
vladislav <ve@incsub.com>
2023-05-12 17:05:46 +0200

    Add some env tests

--- pkg/env/name_test.go
+++ pkg/env/name_test.go
@@ -1,1 +1,20 @@
 package env
+
+import "testing"
+
+func Test_NameString(t *testing.T) {
+	suite := map[string]Name{
+		"AVBL_SITE_ID":         SiteID,
+		"AVBL_SITE_URL":        SiteURL,
+		"AVBL_PREVIOUSLY_DOWN": PreviouslyDown,
+		"AVBL_DBCONN_URI":      DBConnURI,
+	}
+	for want, test := range suite {
+		t.Run(want, func(t *testing.T) {
+			got := test.String()
+			if want != got {
+				t.Errorf("want %q, got %q", want, got)
+			}
+		})
+	}
+}
