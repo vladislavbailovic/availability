8537ebc162a4f0fce7c5e743d2558d3b3dba7cba
vladislav <ve@incsub.com>
2023-05-13 20:20:34 +0200

    Register handlers

--- cmd/data-api-server/main.go
+++ cmd/data-api-server/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"availability/pkg/env"
+	"errors"
+	"fmt"
 	"log"
+	"net/http"
+
+	"availability/pkg/env"
+	"availability/pkg/server"
 )
 
 func main() {
 	apiPort := env.ApiPortData.WithFallback("3667")
 	auth := env.ApiSecretData.Value()
 
-	log.Println(apiPort, auth)
+	hdr := http.Header{}
+	if auth != "" {
+		hdr.Add("x-avbl-auth", auth)
+	}
+	registerHandlers(hdr)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", apiPort), nil))
+}
+
+func registerHandlers(hdr http.Header) {
+	http.HandleFunc("/since/", server.Handle(server.WithExpectedHeaders(
+		hdr, server.WithExpectedMethod(http.MethodGet, since))))
+	http.HandleFunc("/", server.Handle(server.WithExpectedHeaders(
+		hdr, server.WithExpectedMethod(http.MethodGet, sourcePeriod))))
+}
+
+func since(w *server.Response, r *http.Request) error {
+	return errors.New("since")
+}
+
+func sourcePeriod(w *server.Response, r *http.Request) error {
+	return errors.New("sourcePeriod")
 }
