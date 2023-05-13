71e5aa1734e03675edc3195bfbc48ac1ea689540
vladislav <ve@incsub.com>
2023-05-13 07:06:20 +0200

    More tests

--- pkg/data/model/source_test.go
+++ pkg/data/model/source_test.go
@@ -13,3 +13,15 @@ func Test_Source_IsValid(t *testing.T) {
 		t.Error("expected invalid")
 	}
 }
+
+func Test_NewSource_IsValid(t *testing.T) {
+	valid := &NewSource{SiteID: 1312, URL: "test"}
+	if !valid.IsValid() {
+		t.Error("expected valid")
+	}
+
+	invalid := new(NewSource)
+	if invalid.IsValid() {
+		t.Error("expected invalid")
+	}
+}
